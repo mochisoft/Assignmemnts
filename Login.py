3a69a9ff2e1b1d3898a06e6178c03756c5cf0fdb
Michael Ochieng <mochisoft@gmail.com>
2017-10-06 19:08:10 +0300

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,1 +1,33 @@
 
+
+def authenticate(pwd1,pwd2):
+    if (pwd1 != pwd2):
+        return False
+    else:
+        return True
+
+def getName():
+    name = raw_input("Please Enter Your Name:\n")
+    return name
+
+def getPassword():
+    pwd2 = raw_input("Please Enter Your Password:\n")
+    return pwd2
+
+if __name__=="__main__":
+    usr=getName()
+    pwd2=getPassword()
+    num_attempts=3
+    pwd1 = "Mike1"
+
+    for x in range(num_attempts):
+        authenticate(pwd1, pwd2)
+        if authenticate(pwd1,pwd2):
+            print "My name is " + usr + " and password is " + pwd1
+            break
+        else:
+            print "" + str(num_attempts - x) + " attempts left. Please try again"
+            usr = getName()
+            pwd2 = getPassword()
+
+
